d4977cb942e5ff66e39b97b8306af9e6759b2fa8
Morgan Ericsson <morganericsson@gmail.com>
2013-03-20 16:34:46 +0100

    minor fixes

--- HelloName/src/com/example/helloname/MainActivity.java
+++ HelloName/src/com/example/helloname/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.helloname;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.EditText;
 
 public class MainActivity extends Activity
 {
@@ -12,4 +15,11 @@ public class MainActivity extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+
+    public void sendName(View view)
+    {
+    	TextView dn = (TextView)findViewById(R.id.displayName);
+    	dn.setText("Hello "+((EditText)findViewById(R.id.enteredName)).getText().toString());
+    }
+
 }
