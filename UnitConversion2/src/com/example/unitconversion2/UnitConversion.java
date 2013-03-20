a0cee91e04d8d4a715a7ba88dbcf81d207703672
Morgan Ericsson <morganericsson@gmail.com>
2013-03-20 17:40:18 +0100

    added intents to unitconversion2

--- UnitConversion2/src/com/example/unitconversion2/UnitConversion.java
+++ UnitConversion2/src/com/example/unitconversion2/UnitConversion.java
@@ -3,8 +3,11 @@ package com.example.unitconversion2;
 import android.app.Activity;
 import android.os.Bundle;
 
+import android.content.Intent;
+
 import android.view.View;
 import android.widget.Spinner;
+
 import android.widget.ArrayAdapter;
 import android.widget.AdapterView;
 import android.widget.EditText;
@@ -12,6 +15,11 @@ import android.widget.AdapterView.OnItemSelectedListener;
 
 public class UnitConversion extends Activity implements OnItemSelectedListener
 {
+	public static final int CV_TEMP = 0;
+	public static final int CV_DIST = 1;
+
+	int type;
+
 	double v1, v2;
 
     /** Called when the activity is first created. */
@@ -19,17 +27,30 @@ public class UnitConversion extends Activity implements OnItemSelectedListener
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.conversion);
 
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-	        R.array.tempUnits, android.R.layout.simple_spinner_item);
+        Intent intent = getIntent();
+        type = intent.getIntExtra(MainActivity.CV_TYPE, CV_TEMP);
+
+        ArrayAdapter<CharSequence> myadapter;
+        switch(type) {
+	        case CV_DIST:
+	           	myadapter = ArrayAdapter.createFromResource(this,
+		        	R.array.distUnits, android.R.layout.simple_spinner_item);
+		        break;
+        	case CV_TEMP:
+        	default:
+	        	myadapter = ArrayAdapter.createFromResource(this,
+		        	R.array.tempUnits, android.R.layout.simple_spinner_item);
+		        break;
+		}
 
         Spinner spinner01 = (Spinner) findViewById(R.id.unit01);
         Spinner spinner02 = (Spinner) findViewById(R.id.unit02);
 
-		spinner01.setAdapter(adapter);
+		spinner01.setAdapter(myadapter);
 		spinner01.setOnItemSelectedListener(this);
-		spinner02.setAdapter(adapter);
+		spinner02.setAdapter(myadapter);
 		spinner02.setOnItemSelectedListener(this);
 
     }
@@ -39,7 +60,7 @@ public class UnitConversion extends Activity implements OnItemSelectedListener
 		int from = position, to;
 		EditText v;
 
-		switch(parentView.getId()) {
+/*		switch(parentView.getId()) {
 			case R.id.unit01:
 				v = (EditText)findViewById(R.id.value01);
 				if(v.getText().length()==0) break;
@@ -56,7 +77,7 @@ public class UnitConversion extends Activity implements OnItemSelectedListener
 				v1 = (TempConverter.convert(v2, from, to));
 				((EditText)findViewById(R.id.value01)).setText(Double.toString(v1));
 				break;
-		}
+		} */
 
 	}
 
