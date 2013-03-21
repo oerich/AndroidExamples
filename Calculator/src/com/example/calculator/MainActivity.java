1bc229c9414f9ea6b362f4adadd9f3bde03f115a
Morgan Ericsson <morganericsson@gmail.com>
2013-03-21 18:44:26 +0100

    Calculator without onClick in XML

--- Calculator/src/com/example/calculator/MainActivity.java
+++ Calculator/src/com/example/calculator/MainActivity.java
@@ -6,13 +6,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Button;
 
 public class MainActivity extends Activity
 {
-	StringBuilder val = null;
-	Double res = null;
-	int op;
-	boolean lastop = false;
+
+	OperatorListener ol = new OperatorListener();
+	DigitListener dl = new DigitListener();
 
     /** Called when the activity is first created. */
     @Override
@@ -20,85 +20,26 @@ public class MainActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-    }
 
-    private double calc(double v1, double v2, int op) {
-    	switch(op) {
-	    case R.id.opPlus:
-	    	return v1 + v2;
-	    case R.id.opMinus:
-	    	return v1 - v2;
-	    case R.id.opMult:
-	    	return v1 * v2;
-	    case R.id.opDiv:
-	    	return v1 / v2;
-	    }
-	    return 0.0; // should never happen
-    }
+        // Set listeners for operators
+        ((Button)findViewById(R.id.opPlus)).setOnClickListener(ol);
+        ((Button)findViewById(R.id.opMinus)).setOnClickListener(ol);
+        ((Button)findViewById(R.id.opMult)).setOnClickListener(ol);
+        ((Button)findViewById(R.id.opDiv)).setOnClickListener(ol);
+        ((Button)findViewById(R.id.opEq)).setOnClickListener(ol);
 
-    public void enterDigit(View view) {
-    	TextView tv = (TextView)findViewById(R.id.val);
-
-    	if(val == null || view.getId() == R.id.clear || lastop == true) {
-    		lastop = false;
-    		val = new StringBuilder("");
-    	}
-
-    	switch(view.getId()) {
-    		case R.id.val1:
-    			val.append("1");
-    			break;
-    		case R.id.val2:
-    			val.append("2");
-    			break;
-    		case R.id.val3:
-    			val.append("3");
-    			break;
-    		case R.id.val4:
-    			val.append("4");
-    			break;
-    		case R.id.val5:
-    			val.append("5");
-    			break;
-    		case R.id.val6:
-    			val.append("6");
-    			break;
-    		case R.id.val7:
-    			val.append("7");
-    			break;
-    		case R.id.val8:
-    			val.append("8");
-    			break;
-    		case R.id.val9:
-    			val.append("9");
-    			break;
-    		case R.id.val0:
-    			val.append("0");
-    			break;
-    		case R.id.dot:
-    			val.append(".");
-    	}
-    	tv.setText(val.toString());
-    }
-
-    public void doCalc(View view) {
-    	lastop = true;
-    	if(res == null) {
-    		res = Double.parseDouble(val.toString());
-    		op = view.getId();
-    	} else {
-    		if(view.getId() == R.id.opEq && val != null) {
-				res = calc(res, Double.parseDouble(val.toString()), op);
-				op = -1;
-				val = new StringBuilder(Double.toString(res));
-				res = null;
-			} else if(op!=-1 && val != null){
-				res = calc(res, Double.parseDouble(val.toString()), view.getId());
-				op = view.getId();
-				val = new StringBuilder(Double.toString(res));
-			}
-			if(val!=null)
-				((TextView)findViewById(R.id.val)).setText(val.toString());
-		}
+        // Set listeners for digits
+        ((Button)findViewById(R.id.val1)).setOnClickListener(dl);
+        ((Button)findViewById(R.id.val2)).setOnClickListener(dl);
+        ((Button)findViewById(R.id.val3)).setOnClickListener(dl);
+        ((Button)findViewById(R.id.val4)).setOnClickListener(dl);
+        ((Button)findViewById(R.id.val5)).setOnClickListener(dl);
+        ((Button)findViewById(R.id.val6)).setOnClickListener(dl);
+        ((Button)findViewById(R.id.val7)).setOnClickListener(dl);
+        ((Button)findViewById(R.id.val8)).setOnClickListener(dl);
+        ((Button)findViewById(R.id.val9)).setOnClickListener(dl);
+        ((Button)findViewById(R.id.val0)).setOnClickListener(dl);
+        ((Button)findViewById(R.id.dot)).setOnClickListener(dl);
+        ((Button)findViewById(R.id.clear)).setOnClickListener(dl);
     }
 }
